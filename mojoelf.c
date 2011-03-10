f497a4a47187f5a6e7a9441021eb207f5dbf474e
Ryan C. Gordon <icculus@icculus.org>
2011-03-10 13:16:41 -0800

    Adjust some predeclares.

--- mojoelf.c
+++ mojoelf.c
@@ -16,10 +16,11 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-void *MOJOELF_dlsym(void *lib, const char *sym);
-void MOJOELF_dlclose(void *lib);
 void *MOJOELF_dlopen_mem(const void *buf, const long buflen);
 void *MOJOELF_dlopen_file(const char *fname);
+void *MOJOELF_dlsym(void *lib, const char *sym);
+void MOJOELF_dlclose(void *lib);
+const char *MOJOELF_dlerror(void);
 #ifdef __cplusplus
 }
 #endif
