6796e4c6f88b25a2a043ba02d522bc8201a03c20
Ryan C. Gordon <icculus@icculus.org>
2011-03-10 22:04:04 -0800

    Enable -ggdb3 debug symbols.

--- make.sh
+++ make.sh
@@ -3,6 +3,6 @@
 set -e
 set -x
 
-gcc -DMOJOELF_TEST=1 -Wall -O0 -g -o mojoelf mojoelf.c -ldl
+gcc -DMOJOELF_TEST=1 -Wall -O0 -ggdb3 -o mojoelf mojoelf.c -ldl
 gcc -fPIC -shared -Wall -O0 -g -o hello.so hello.c
 
