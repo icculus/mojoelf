275d0a442155b3421b3ba61b73de6f8bb5a8e799
Ryan C. Gordon <icculus@icculus.org>
2011-03-10 22:29:03 -0500

    Need -ldl on Linux.

--- make.sh
+++ make.sh
@@ -3,6 +3,6 @@
 set -e
 set -x
 
-gcc -DMOJOELF_TEST=1 -Wall -O0 -g -o mojoelf mojoelf.c
+gcc -DMOJOELF_TEST=1 -Wall -O0 -g -o mojoelf mojoelf.c -ldl
 gcc -fPIC -shared -Wall -O0 -g -o hello.so hello.c
 
