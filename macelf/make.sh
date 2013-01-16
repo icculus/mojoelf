03698e10dadf548220ea66ffe56b47973109ebcb
Ryan C. Gordon <icculus@icculus.org>
2013-01-16 01:42:39 -0500

    Switched over to clang for now.

--- macelf/make.sh
+++ macelf/make.sh
@@ -1,27 +1,27 @@
 mkdir -p bin
-gcc -o bin/mojoelf.o \
-    -c -arch i386 \
-    -I.. -O0 -ggdb3 -Wall -pipe \
+clang -o bin/mojoelf.o \
+    -arch i386 -c \
+    -I.. -O0 -g -Wall -pipe \
     -DMOJOELF_SUPPORT_DLERROR=1 \
     -DMOJOELF_SUPPORT_DLOPEN_FILE=1 \
     -DMOJOELF_ALLOW_SYSTEM_RESOLVE=0 \
     -DMOJOELF_REDUCE_LIBC_DEPENDENCIES=0 \
     ../mojoelf.c
 
-gcc -o bin/mactrampolines.o \
-    -c -arch i386 -mstackrealign \
-    -I.. -I. -O0 -ggdb3 -Wall -pipe \
+clang -o bin/mactrampolines.o \
+    -arch i386 -c -mstackrealign \
+    -I.. -I. -O0 -g -Wall -pipe \
     ./mactrampolines.c
 
-gcc -o bin/macelf.o \
-    -c -arch i386 \
-    -I.. -I. -O0 -ggdb3 -Wall -pipe \
+clang -o bin/macelf.o \
+    -arch i386 -c \
+    -I.. -I. -O0 -g -Wall -pipe \
     ./macelf.c
 
-gcc -o bin/hashtable.o \
-    -c -arch i386 \
-    -I.. -I. -O0 -ggdb3 -Wall -pipe \
+clang -o bin/hashtable.o \
+    -arch i386 -c \
+    -I.. -I. -O0 -g -Wall -pipe \
     ./hashtable.c
 
-gcc -arch i386 -o macelf bin/mojoelf.o bin/mactrampolines.o bin/macelf.o bin/hashtable.o -liconv
+clang -arch i386 -g -o macelf bin/mojoelf.o bin/mactrampolines.o bin/macelf.o bin/hashtable.o -liconv
 
